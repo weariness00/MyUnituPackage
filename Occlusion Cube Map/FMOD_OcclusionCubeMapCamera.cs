b2e0f696811a474d3a73e2d44e83b48e20a4ee7e
Seo Dong Woo <ehddn917@gmail.com>
2025-06-27 12:56:28 +0900

    test: 큐브맵 기반 Occlusion 제작 중

---
+++ Occlusion Cube Map/FMOD_OcclusionCubeMapCamera.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Weariness.FMOD.Occlusion
+{
+    [AddComponentMenu("FMOD Studio/Occlusion/Cube Map Camera")]
+    public class FMOD_OcclusionCubeMapCamera : MonoBehaviour
+    {
+        public Camera cubeMapCamera;
+        
+        public void Awake()
+        {
+            // 큐브맵 RenderTexture 생성 (예: 128x128 해상도)
+            var cubeRT = new RenderTexture(128, 128, 16);
+            cubeRT.dimension = UnityEngine.Rendering.TextureDimension.Cube;
+            cubeRT.hideFlags = HideFlags.DontSave;
+            cubeRT.Create();
+            
+            // 카메라 세팅
+            cubeMapCamera.clearFlags = CameraClearFlags.SolidColor;
+            cubeMapCamera.backgroundColor = Color.black; // 배경 = 완전 투명/무음
+            cubeMapCamera.enabled = false; // 수동 렌더링만 사용
+        }
+
+        public void RenderOcclusionTexture()
+        {
+            
+        }
+    }
+}
---
+++ Occlusion Cube Map/FMOD_OcclusionObstacle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Weariness.FMOD.Occlusion
+{
+    public partial class FMOD_OcclusionObstacle
+    {
+        private static Material SharedOcclusionMat;
+        private static bool isInit = false;
+
+        private static void Init()
+        {
+            if (SharedOcclusionMat == null)
+            {
+                // "OcclusionMaskUnlit"은 .mat 파일명 (확장자 제외)
+                SharedOcclusionMat = Resources.Load<Material>("OcclusionMaskUnlit");
+                if (SharedOcclusionMat == null)
+                    Debug.LogError("OcclusionMaskUnlit 머티리얼을 Resources 폴더에 넣으세요!");
+            }
+        }
+    }
+    
+    public partial class FMOD_OcclusionObstacle : MonoBehaviour
+    {
+        private static int OcclusionID = Shader.PropertyToID("_Occlusion");
+        
+        [Range(0,1)] public float occlusionStrength = 1f; // 장애물의 음향 차단 강도
+        
+        private Renderer renderer;
+        private MaterialPropertyBlock mpb;
+
+#if UNITY_EDITOR
+        [NonSerialized] private float prevOcclusionStrength = 1f; // 이전 occlusionStrength 값, 에디터에서만 사용
+#endif
+
+        public void OnValidate()
+        {
+            if (EditorApplication.isPlaying)
+            {
+                if (Math.Abs(occlusionStrength - prevOcclusionStrength) > 0.001f)
+                {
+                    SetOcclusion(occlusionStrength);
+                }
+            }
+        }
+
+        void Awake()
+        {
+#if UNITY_EDITOR
+            prevOcclusionStrength = occlusionStrength; // 이전 occlusionStrength 값, 에디터에서만 사용
+#endif
+            renderer = GetComponent<Renderer>();
+            Debug.Assert(renderer != null, "Renderer가 존재 하지 않아 OcclusionObstacle이 동작하지 않습니다.");
+            
+            mpb = new MaterialPropertyBlock();
+            renderer.GetPropertyBlock(mpb);
+            mpb.SetFloat(OcclusionID, occlusionStrength);
+            renderer.SetPropertyBlock(mpb);
+        }
+
+        // 값이 바뀔 때마다 갱신하고 싶으면 함수로 따로 빼서 호출
+        public void SetOcclusion(float value)
+        {
+#if true
+            prevOcclusionStrength = value;    
+#endif
+            
+            occlusionStrength = value;
+            mpb.SetFloat(OcclusionID, value);
+            renderer.SetPropertyBlock(mpb);
+        }
+    }
+}
