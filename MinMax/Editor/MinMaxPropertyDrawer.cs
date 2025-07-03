8e9b0f9092b57ab473f9adcf8cf5d5b608c4ce0d
Seo Dong Woo <ehddn917@gmail.com>
2025-07-03 11:29:56 +0900

    feat: min max에 vector2,vector3도 표시 되도록 추가
    수정사항
    - float Random 추가
    - Vector2, Vector3가 인스펙터에 표시 되도록 추가

--- MinMax/Editor/MinMaxPropertyDrawer.cs
+++ MinMax/Editor/MinMaxPropertyDrawer.cs
@@ -31,10 +31,11 @@ namespace Weariness.Util.Editor
             EditorGUI.indentLevel = 0;
 
             float sumInterval = 0;
+            float charWidth = 9f; // 에디터 폰트 기준 대략 문자 1자당 폭
 
             // Min Value 필드
             int minDigitCount = min.intValue.GetDigitCount();
-            minValueInterval = minDigitCount <= 2 ? 25 : minDigitCount * 11;
+            minValueInterval = Mathf.Clamp(minDigitCount * charWidth + 5f, 25f, 100f);
             var minPos = new Rect(position.x + sumInterval, position.y, minValueInterval, position.height);
             min.intValue = EditorGUI.IntField(minPos, min.intValue);
             sumInterval += minValueInterval;
@@ -45,7 +46,7 @@ namespace Weariness.Util.Editor
             sumInterval += textInterval;
 
             int maxDigitCount = max.intValue.GetDigitCount();
-            maxValueInterval = maxDigitCount <= 2 ? 25 : maxDigitCount * 11;
+            maxValueInterval = Mathf.Clamp(maxDigitCount * charWidth + 5f, 25f, 100f);
             var maxPos = new Rect(position.x + sumInterval, position.y, maxValueInterval, position.height);
             max.intValue = EditorGUI.IntField(maxPos, max.intValue);
             sumInterval += maxValueInterval;
@@ -86,10 +87,11 @@ namespace Weariness.Util.Editor
             EditorGUI.indentLevel = 0;
 
             float sumInterval = 0; // 전체 간격 길이
+            float charWidth = 9f; // 에디터 폰트 기준 대략 문자 1자당 폭
 
             // min value 필드 값
             int minDigitCount = min.floatValue.GetDigitCount();
-            minValueInterval = minDigitCount <= 2 ? 25 : minDigitCount * 9;
+            minValueInterval = Mathf.Clamp(minDigitCount * charWidth + 5f, 25f, 100f);
             var minPos = new Rect(position.x + sumInterval, position.y, minValueInterval, position.height);
             min.floatValue = EditorGUI.FloatField(minPos, min.floatValue);
             sumInterval += minValueInterval;
@@ -102,7 +104,7 @@ namespace Weariness.Util.Editor
 
             // Max Value 필드 값
             int maxDigitCount = max.floatValue.GetDigitCount();
-            maxValueInterval = maxDigitCount <= 2 ? 25 : maxDigitCount * 9;
+            maxValueInterval = Mathf.Clamp(maxDigitCount * charWidth + 5f, 25f, 100f);
             var maxPos = new Rect(position.x + sumInterval, position.y, maxValueInterval, position.height);
             max.floatValue = EditorGUI.FloatField(maxPos, max.floatValue);
             sumInterval += maxValueInterval;
@@ -111,6 +113,232 @@ namespace Weariness.Util.Editor
             EditorGUI.EndProperty();
 
             property.serializedObject.ApplyModifiedProperties();
+        }
+    }    
+    
+    [CustomPropertyDrawer(typeof(MinMax<Vector2>))]
+    public class MinMaxVector2PropertyDrawer : PropertyDrawer
+    {
+        float commaWidth = 8f;
+        float charWidth = 9f;
+        float minTotalWidth = 0;
+        float maxTotalWidth = 0;
+
+        private float maxWidthLength = 150f;
+
+        int size = 2;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            var minProp = property.FindPropertyRelative("_min");
+            var maxProp = property.FindPropertyRelative("_max");
+
+            Vector3 min = minProp.vector2Value;
+            Vector3 max = maxProp.vector2Value;
+
+            position = EditorGUI.PrefixLabel(position, label);
+
+
+            // min/max 칸 크기 계산
+            float[] minWidths = new float[3];
+            float[] maxWidths = new float[3];
+
+            minTotalWidth = 16;
+            maxTotalWidth = 16;
+            for (int i = 0; i < size; ++i)
+            {
+                minWidths[i] = Mathf.Clamp(min[i].GetDigitCount() * charWidth + 5f, 25f, 100f);
+                maxWidths[i] = Mathf.Clamp(max[i].GetDigitCount() * charWidth + 5f, 25f, 100f);
+                if (i != 0)
+                {
+                    minTotalWidth += commaWidth;
+                    maxTotalWidth += commaWidth;
+                }
+
+                minTotalWidth += minWidths[i];
+                maxTotalWidth += maxWidths[i];
+            }
+
+            float xMin = position.x;
+            float xMax = position.x;
+            float yMin = position.y;
+            float yMax = position.y;
+
+            float lineHeight = EditorGUIUtility.singleLineHeight;
+
+            // 한 줄에 다 못들어가면 2줄로 분리
+            bool splitLine = minTotalWidth > maxWidthLength || maxTotalWidth > maxWidthLength;
+
+            // min 필드
+            for (int i = 0; i < size; ++i)
+            {
+                min[i] = EditorGUI.FloatField(new Rect(xMin, yMin, minWidths[i], lineHeight), min[i]);
+                xMin += minWidths[i];
+                if (i != size - 1)
+                {
+                    xMin += commaWidth;
+                }
+            }
+
+            // ~
+            EditorGUI.LabelField(new Rect(xMin, yMin, 18, lineHeight), " ~ ");
+
+            if (splitLine)
+            {
+                // max를 다음 줄에
+                yMax += lineHeight + EditorGUIUtility.standardVerticalSpacing;
+                xMax = position.x;
+            }
+            else
+            {
+                // max는 같은 줄에
+                xMax = xMin + 22;
+            }
+
+
+            // max 필드
+            for (int i = 0; i < size; ++i)
+            {
+                max[i] = EditorGUI.FloatField(new Rect(xMax, yMax, maxWidths[i], lineHeight), max[i]);
+                xMax += maxWidths[i];
+                if (i != size - 1)
+                {
+                    xMax += commaWidth;
+                }
+            }
+
+            minProp.vector2Value = min;
+            maxProp.vector2Value = max;
+
+            EditorGUI.EndProperty();
+            property.serializedObject.ApplyModifiedProperties();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (minTotalWidth > maxWidthLength || maxTotalWidth > maxWidthLength)
+            {
+                // 두 줄
+                return EditorGUIUtility.singleLineHeight * 2 + EditorGUIUtility.standardVerticalSpacing;
+            }
+
+            // 한 줄
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+
+    [CustomPropertyDrawer(typeof(MinMax<Vector3>))]
+    public class MinMaxVector3PropertyDrawer : PropertyDrawer
+    {
+        float commaWidth = 8f;
+        float charWidth = 9f;
+        float minTotalWidth = 0;
+        float maxTotalWidth = 0;
+
+        private float maxWidthLength = 150f;
+
+        int size = 3;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            var minProp = property.FindPropertyRelative("_min");
+            var maxProp = property.FindPropertyRelative("_max");
+
+            Vector3 min = minProp.vector3Value;
+            Vector3 max = maxProp.vector3Value;
+
+            position = EditorGUI.PrefixLabel(position, label);
+
+
+            // min/max 칸 크기 계산
+            float[] minWidths = new float[3];
+            float[] maxWidths = new float[3];
+
+            minTotalWidth = 16;
+            maxTotalWidth = 16;
+            for (int i = 0; i < size; ++i)
+            {
+                minWidths[i] = Mathf.Clamp(min[i].GetDigitCount() * charWidth + 5f, 25f, 100f);
+                maxWidths[i] = Mathf.Clamp(max[i].GetDigitCount() * charWidth + 5f, 25f, 100f);
+                if (i != 0)
+                {
+                    minTotalWidth += commaWidth;
+                    maxTotalWidth += commaWidth;
+                }
+
+                minTotalWidth += minWidths[i];
+                maxTotalWidth += maxWidths[i];
+            }
+
+            float xMin = position.x;
+            float xMax = position.x;
+            float yMin = position.y;
+            float yMax = position.y;
+
+            float lineHeight = EditorGUIUtility.singleLineHeight;
+
+            // 한 줄에 다 못들어가면 2줄로 분리
+            bool splitLine = minTotalWidth > maxWidthLength || maxTotalWidth > maxWidthLength;
+
+            // min 필드
+            for (int i = 0; i < size; ++i)
+            {
+                min[i] = EditorGUI.FloatField(new Rect(xMin, yMin, minWidths[i], lineHeight), min[i]);
+                xMin += minWidths[i];
+                if (i != size - 1)
+                {
+                    xMin += commaWidth;
+                }
+            }
+
+            // ~
+            EditorGUI.LabelField(new Rect(xMin, yMin, 18, lineHeight), " ~ ");
+
+            if (splitLine)
+            {
+                // max를 다음 줄에
+                yMax += lineHeight + EditorGUIUtility.standardVerticalSpacing;
+                xMax = position.x;
+            }
+            else
+            {
+                // max는 같은 줄에
+                xMax = xMin + 22;
+            }
+
+
+            // max 필드
+            for (int i = 0; i < size; ++i)
+            {
+                max[i] = EditorGUI.FloatField(new Rect(xMax, yMax, maxWidths[i], lineHeight), max[i]);
+                xMax += maxWidths[i];
+                if (i != size - 1)
+                {
+                    xMax += commaWidth;
+                }
+            }
+
+            minProp.vector3Value = min;
+            maxProp.vector3Value = max;
+
+            EditorGUI.EndProperty();
+            property.serializedObject.ApplyModifiedProperties();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (minTotalWidth > maxWidthLength || maxTotalWidth > maxWidthLength)
+            {
+                // 두 줄
+                return EditorGUIUtility.singleLineHeight * 2 + EditorGUIUtility.standardVerticalSpacing;
+            }
+
+            // 한 줄
+            return EditorGUIUtility.singleLineHeight;
         }
     }
 }
--- MinMax/MinMax.cs
+++ MinMax/MinMax.cs
@@ -1,55 +1,98 @@
-using UnityEngine;
+    using UnityEngine;
 
-namespace Weariness.Util
-{
-    [System.Serializable]
-    public class MinMax<T>
+    namespace Weariness.Util
     {
-        [SerializeField] private T _min;
-        [SerializeField] private T _max;
+        [System.Serializable]
+        public class MinMax<T>
+        {
+            [SerializeField] private T _min;
+            [SerializeField] private T _max;
+
+            public T Min
+            {
+                get => _min;
+                set => _min = value;
+            }
+
+            public T Max
+            {
+                get => _max;
+                set => _max = value;
+            }
+
+            public MinMax(T min, T max)
+            {
+                _min = min;
+                _max = max;
+            }
+        }
         
-        public T Min
+        // int
+        public static partial class MinMaxExtension
         {
-            get => _min;
-            set => _min = value;
-        }
+            public static int Length(this MinMax<int> value)
+            {
+                return Mathf.Abs(value.Min) + Mathf.Abs(value.Max);
+            }
 
-        public T Max
-        {
-            get => _max;
-            set => _max = value;
+            public static int Random(this MinMax<int> value, bool includeMax =false)
+            {
+                return UnityEngine.Random.Range(value.Min, value.Max + (includeMax ? 1 : 0));
+            }
         }
         
-        public MinMax(T min, T max)
+        // float
+        public static partial class MinMaxExtension
         {
-            _min = min;
-            _max = max;
+            public static int Length(this MinMax<float> value)
+            {
+                return (int)(Mathf.Abs(value.Min) + Mathf.Abs(value.Max));
+            }
+
+            public static float Random(this MinMax<float> value)
+            {
+                return UnityEngine.Random.Range(value.Min, value.Max);
+            }
+        }
+        
+        // Vector2
+        public static partial class MinMaxExtension
+        {
+            public static Vector2Int Length(this MinMax<Vector2> value)
+            {
+                Vector2Int length = Vector2Int.zero;
+                length.x = Mathf.Abs((int)value.Min.x) + Mathf.Abs((int)value.Max.x);
+                length.y = Mathf.Abs((int)value.Min.y) + Mathf.Abs((int)value.Max.y);
+                return length;
+            }
+
+            public static Vector2 Random(this MinMax<Vector2> value)
+            {
+                Vector2 random = Vector2.zero;
+                random.x = UnityEngine.Random.Range(value.Min.x, value.Max.x);
+                random.y = UnityEngine.Random.Range(value.Min.x, value.Max.x);
+                return random;
+            }
+        }
+        
+        // Vector3
+        public static partial class MinMaxExtension
+        {
+            public static Vector3Int Length(this MinMax<Vector3> value)
+            {
+                Vector3Int length = Vector3Int.zero;
+                length.x = Mathf.Abs((int)value.Min.x) + Mathf.Abs((int)value.Max.x);
+                length.y = Mathf.Abs((int)value.Min.y) + Mathf.Abs((int)value.Max.y);
+                return length;
+            }
+
+            public static Vector3 Random(this MinMax<Vector3> value)
+            {
+                Vector3 random = Vector3.zero;
+                random.x = UnityEngine.Random.Range(value.Min.x, value.Max.x);
+                random.y = UnityEngine.Random.Range(value.Min.x, value.Max.x);
+                random.z = UnityEngine.Random.Range(value.Min.z, value.Max.z);
+                return random;
+            }
         }
     }
-    
-    public static class MinMaxIntExtension
-    {
-        public static int Length(this MinMax<int> value)
-        {
-            return Mathf.Abs(value.Min) + Mathf.Abs(value.Max);
-        }
-
-        public static int Random(this MinMax<int> value, bool includeMax =false)
-        {
-            return UnityEngine.Random.Range(value.Min, value.Max + (includeMax ? 1 : 0));
-        }
-    }
-
-    public static class MinMaxFloatExtension
-    {
-        public static int Length(this MinMax<float> value)
-        {
-            return (int)(Mathf.Abs(value.Min) + Mathf.Abs(value.Max));
-        }
-
-        public static int Random(this MinMax<float> value)
-        {
-            return UnityEngine.Random.Range(value.Min, value.Max);
-        }
-    }
-}
