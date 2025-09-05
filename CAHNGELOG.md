99bf9eb358d2d4667413f8fceff1fedfedce2582
Seo Dong Woo <ehddn917@gmail.com>
2025-09-05 11:52:53 +0900

    feat: TransformExtension 추가
    LookAt 2D 구현

--- CAHNGELOG.md
+++ CAHNGELOG.md
@@ -1,8 +1,8 @@
 # [1.0.11]
-## 2025-08-26
+## 2025-09-05
 ### 추가 사항
 - Stat자체를 Modifier로 사용가능하도록 하는 Stat.AsModifier(StatModifier.ModifierType modifierType) 함수를 구현
-
+- TransformExtension을 제작, 2d 환경에서는 바라보는 방향으로 인한 회전이 z축에서만 일어나야하는 것에 대한 LookAt2D 함수를 구현
 ### 수정 사항
 - Stat 클래스에 BaseValue 속성 추가
 - Stat 클래스에 CachedValue를 추가하여 Value 속성 호출시 마다. 비용이 큰 GetValue 메서드 호출을 방지
---
+++ Runtime/Extensions/TransformExtension.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Weariness.Util.Extensions
+{
+    public static class TransformExtension
+    {
+        public static void LookAt2D(this Transform transform, Transform target) => LookAt2D(transform, target.position);
+        public static void LookAt2D(this Transform transform, Vector3 targetPosition)
+        {
+            Vector2 dir = (targetPosition - transform.position).normalized;
+
+            // 오른쪽을 기준으로 한 signed angle
+            float angle = Vector2.SignedAngle(Vector2.right, dir);
+
+            // flip 여부 판단
+            bool flip = (angle > 90f) || (angle < -90f);
+
+            // Y축 회전
+            float yRot = flip ? 180f : 0f;
+
+            // Z축 회전 (flip일 때는 180 기준으로 보정)
+            float zRot = flip ? Mathf.DeltaAngle(angle, 180f) : angle;
+
+            // 오일러 회전에 의해 z값 회전이 안먹히고 x값이 변화하는 것을 방지하기 위해 y를 먼저 회전후 z 회전
+            transform.localRotation = Quaternion.AngleAxis(yRot, Vector3.up) * 
+                                      Quaternion.AngleAxis(zRot, Vector3.forward);
+        }
+    }
+}
